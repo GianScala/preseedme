3c9e5fb24af3e9661f8bd54cde3b22794efac2fe
GianScala <gm.scalabrin@gmail.com>
2025-12-08 23:30:04 -0800

    updated policy & terms

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function PrivacyPage() {
   return (
-    <main className="min-h-screen text-neutral-200 pt-10 pb-4">
+    <main className="min-h-screen text-neutral-200 pt-4 pb-4">
       <div className="px-4 sm:px-6 lg:px-8 space-y-8">
         
         {/* Header */}
